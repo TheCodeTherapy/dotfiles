5decbc0b5d56480df04458ef3006d297b1af6102
mgzme <me@mgz.me>
2019-09-08 12:05:03 +0100

    Including network-manager-applet to config.sh

--- config.sh
+++ config.sh
@@ -12,7 +12,7 @@ sudo pacman -S --noconfirm --needed tmux powerline powerline-common \
     exa ripgrep bat fd tumbler ffmpegthumbnailer exfat-utils alsa-utils \
     clang ctags chromium rofi mpd ncmpcpp uthash powertop htop unzip xz \
     neofetch feh dunst docker docker-compose opera opera-ffmpeg-codecs \
-    xclip libnotify
+    xclip libnotify network-manager-applet
 
 sudo updatedb
 
